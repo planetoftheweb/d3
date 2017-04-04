375759d210e2fc972bcbdca5c57b1b2685c5ce68
Ray Villalobos <ray@lynda.com>
2017-04-03 20:40:15 -0400

    01_03 Modifying Selections

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,1 +1,7 @@
-d3.selectAll("tr:nth-child(even) .day-high .temp").text("hot");
+d3.selectAll("tr:nth-child(1) .day-high .temp")
+  .html("<strong>hot</strong>")
+  .insert('span', 'strong')
+  .html('today ');
+
+  d3.selectAll('tr:nth-child(3)')
+    .remove();
