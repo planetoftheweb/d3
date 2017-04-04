17edde4d3c31e07f6f13107e8b9753054dc33cde
Ray Villalobos <ray@lynda.com>
2017-04-04 13:10:38 -0400

    01_04e modifying attributes and css

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,7 +1,23 @@
-d3.selectAll("tr:nth-child(1) .day-high .temp")
-  .html("<strong>hot</strong>")
-  .insert('span', 'strong')
-  .html('today ');
+d3.selectAll("tr:nth-child(4) .day-high")
+  .append('span')
+  .html('hot')
 
-  d3.selectAll('tr:nth-child(3)')
-    .remove();
+  // .style('background', 'red')
+  // .style('padding', '3px')
+  // .style('margin-left', '3px')
+  // .style('border-radius', '3px')
+  // .style('color', 'white')
+
+  .classed('label label-danger', true)
+  .classed('label-danger', false)
+  .classed('label-warning', true)
+  .style('margin-left', '3px')
+
+d3.select('h2 span')
+  .classed('small', false)
+
+d3.selectAll('tr')
+  .insert('td', ':first-child')
+  .append('input')
+  .attr('type', 'checkbox')
+  .property('checked', true)
