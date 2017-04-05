b7341487fbb2c8a43e99eb37602973bf95b06478
Ray Villalobos <ray@lynda.com>
2017-04-05 19:51:00 -0400

    01_06e

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,13 +1,17 @@
-d3.selectAll(".day-high .temp")
-  .data([45, 78, 77, 66, 76])
-  .html(function(d, i) {
-    if(i==0) {
-      return '<strong class="text-muted" style="font-size: 2rem">' + d + '</strong>';
-    } else {
-      if (d >= 77) {
-        return '<strong class="text-danger">' + d + '</strong>';
-      } else {
-        return '<strong>' + d + '</strong>';
-      }
-    }
+var mydata = [
+  { date: '4/01/2017', low: 55, high: 78 },
+  { date: '4/02/2017', low: 65, high: 83 },
+  { date: '4/03/2017', low: 77, high: 90 },
+  { date: '4/04/2017', low: 58, high: 78 },
+  { date: '4/05/2017', low: 67, high: 92 },
+];
+
+d3.select('tbody')
+  .selectAll('tr')
+  .data(mydata)
+  .enter().append('tr')
+  .html(function(d) {
+    return '<th scope="row">' + d.date +
+            '</th><td>' + d.low +
+            '</td><td>' + d.high + '</td>'
   })
