642075263775ddea2a154829b45c764a9f3bb54c
Ray Villalobos <ray@lynda.com>
2017-04-06 17:38:26 -0400

    03_02e

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,8 +1,12 @@
-var bardata = [20, 30, 45, 15];
+var bardata = [12, 78, 45, 15];
 var height = 400,
     width = 600,
     barWidth = 50,
     barOffset = 5;
+
+var yScale = d3.scaleLinear()
+    .domain([0, d3.max(bardata)])
+    .range([0,height]);
 
 d3.select('#viz').append('svg')
   .attr('width', width)
@@ -13,11 +17,11 @@ d3.select('#viz').append('svg')
     .style('fill', '#C61C6F')
     .attr('width', barWidth)
     .attr('height', function(d) {
-      return d;
+      return yScale(d);
     })
     .attr('x', function(d, i) {
       return i*(barWidth + barOffset)
     })
     .attr('y', function(d) {
-      return height - d;
+      return height - yScale(d);
     });
