2b229e3c37055a65a3c53ba65eff02372b9e55e3
Ray Villalobos <ray@lynda.com>
2017-04-10 21:41:50 -0400

    03_04e

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,4 +1,7 @@
-var bardata = [12, 78, 45, 15, 45, 66, 24, 64];
+var bardata = [];
+    for (var i = 0; i<100; i++) {
+      bardata.push(Math.random() * 30);
+    }
 var height = 400,
     width = 600,
     barWidth = 50,
@@ -14,13 +17,22 @@ var xScale = d3.scaleBand()
     .paddingOuter(.1)
     .range([0, width])
 
+var colors = d3.scaleLinear()
+    .domain([0, bardata.length *.33,
+                bardata.length *.66,
+                bardata.length
+                ])
+    .range(['#B58929', '#C61C6F',
+            '#268BD2', '#85992C'])
+
 d3.select('#viz').append('svg')
   .attr('width', width)
   .attr('height', height)
-  .style('background', '#C9D7D6')
 .selectAll('rect').data(bardata)
   .enter().append('rect')
-    .style('fill', '#C61C6F')
+    .attr('fill', function(d, i) {
+      return colors(i)
+    })
     .attr('width', function(d) {
       return xScale.bandwidth();
     })
